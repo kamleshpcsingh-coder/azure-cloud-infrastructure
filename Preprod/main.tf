d1b69723b4d117ec16343c98cff64ab7e34c1531
Kamlesh Kumar Singh <kamlesh.pcsingh@mail.com>
2026-08-17 04:42:49 +0530

    update code

--- Preprod/main.tf
+++ Preprod/main.tf
@@ -1,28 +1,28 @@
 module "resource_group" {
-  source = "../../Module/Azurerm_Resource_Group"
+  source = "../Module/Azurerm_Resource_Group"
   rgs    = var.rgs
 }
 
 module "storage_account" {
   depends_on = [module.resource_group]
-  source     = "../../Module/Azurerm_Storage_Account"
+  source     = "../Module/Azurerm_Storage_Account"
   sta        = var.sta
 }
 
 module "container" {
   depends_on = [module.storage_account]
-  source     = "../../Module/Azurerm_Container"
+  source     = "../Module/Azurerm_Container"
   cnt        = var.cnt
 }
 
 module "virtual_network" {
   depends_on = [module.storage_account]
-  source     = "../../Module/Azurerm_Virtual_Network"
+  source     = "../Module/Azurerm_Virtual_Network"
   vnets      = var.vnets
 }
 
 module "subnets" {
   depends_on = [module.storage_account]
-  source     = "../../Module/Azurerm_Subnet"
+  source     = "../Module/Azurerm_Subnet"
   snets      = var.snets
 }
